021e213fbe1a9c144a186e254512cd0ce534cdc0
somchok <nemesic31@gmail.com>
2022-11-21 20:11:29 +0700

    feat(tailwind): config css

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,8 +6,16 @@ import {headerHome,header} from "../../static/images";
 
 export const Home = () => {
   return (<>
-    <div className="text-yellow-400" >
-    hello
+    <div className="flex justify-center " >
+      <img className="w-full absolute z-1 " src={headerHome} />
+      <div className="absolute z-2 flex-col desktop:w-30rem tablet:w-1/2 ">
+      <img src={header} />
+      <button className="w-full mt-1 py-4 rounded-4xl bg-pink hover:bg-green " >
+        <div className="desktop:text-28px tablet:text-18px text-white font-bold">
+        REGISTER NOW. FREE!
+        </div>
+      </button>
+      </div>
     </div>
       </>);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,46 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      '28px': '28px',
+      '18px': '18px',
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'white': '#ffffff',
+      'purple': '#7e5bef',
+      'pink': '#f7418e',
+      'orange': '#ff7849',
+      'green': '#28a745',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '30rem':'30rem',
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
