c701eb6f0b5a1c46e050a25ab9fb9390bbc47d50
annoppanna <annoppanna@gmail.com>
2022-11-27 01:31:39 +0700

    feat(working on navbar component): adding navbar to the app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,71 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import { Col, Image, Row } from "antd";
+import { Link, useLocation } from "react-router-dom";
+import { Spin as Hamburger } from "hamburger-react";
 
 import logo from "../static/images/logo.png";
+import { exportPaths } from "./utils";
+import useOnClickOutside from "./utils/useOnClickOutside";
 
 export const Navbar: React.FC<{}> = () => {
+  const location = useLocation();
+  const ref = React.useRef() as any;
+  const [isOpen, setOpen] = React.useState(false);
+  const [isModalOpen, setModalOpen] = React.useState(false);
+
+  useOnClickOutside(ref, () => setOpen(false));
+
+  const paths = exportPaths(location.pathname);
+
   return (
-    <div className="navbar-header">
-      {/* <Row className="navbar-container">
-        <Col className="logo-container">
-          <Image width={300} src={logo} preview={false} />
-        </Col>
-        <Col className="navbar-content">
-          <Row className="navbar-header-top">
-            Times are shown in your local time zone (UTC+07:00)
-          </Row>
-          <Row className="navbar-header-bottom">
-            <Link to="" className="link-container">
-              about
-            </Link>
-            <Link to="/agenda" className="link-container">
-              agenda
-            </Link>
-            <Link to="/sponsor" className="link-container">
-              sponsor
-            </Link>
-            <Link to="/speaker" className="link-container">
-              speaker
-            </Link>
-          </Row>
-        </Col>
-      </Row> */}
+    <div className="nav-header-content">
+      <div
+        ref={ref}
+        className="menu-overlay"
+        style={{ visibility: isOpen ? "visible" : "hidden" }}
+      >
+        <div className="menu-overlay-content">
+          <div className="menu-link-overlay">
+            {paths.map((item: any) => (
+              <Link key={item.label} to={item.path} className={item.className}>
+                {item.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="navbar-header-sm">
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
+        </div>
+        <div ref={ref}>
+          <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
+        </div>
+      </div>
+
+      <div className="navbar-header-md">
+        <div className="navbar-container">
+          <div className="logo-container">
+            <img width={300} src={logo} alt="logo" />
+          </div>
+          <div className="navbar-content">
+            <div className="navbar-header-top">
+              Times are shown in your local time zone (UTC+07:00)
+            </div>
+            <div className="navbar-header-bottom">
+              {paths.map((item: any) => (
+                <Link
+                  key={item.label}
+                  to={item.path}
+                  className={item.className}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
