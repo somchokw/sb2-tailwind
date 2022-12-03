8073b5e87211af9d9f79e0f0462e583f1f594f82
annoppanna <annoppanna@gmail.com>
2022-12-03 11:27:08 +0700

    feat(adding single speaker): speaker single page

--- src/components/utils/exportLink.tsx
+++ src/components/utils/exportLink.tsx
@@ -3,22 +3,22 @@ export const exportPaths = (location: any) => {
     {
       path: "",
       label: "ABOUT",
-      className: location === "/" ? "link-active" : "link",
+      className: location === "" ? "link-active" : "link",
     },
     {
       path: "/agenda",
       label: "AGENDA",
-      className: location === "/agenda" ? "link-active" : "link",
+      className: location === "agenda" ? "link-active" : "link",
     },
     {
       path: "/sponsor",
       label: "SPONSOR",
-      className: location === "/sponsor" ? "link-active" : "link",
+      className: location === "sponsor" ? "link-active" : "link",
     },
     {
       path: "/speaker",
       label: "SPEAKER",
-      className: location === "/speaker" ? "link-active" : "link",
+      className: location === "speaker" ? "link-active" : "link",
     },
   ];
 
